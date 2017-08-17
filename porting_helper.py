07931d03b8f54d10cc21cb095e9b3f0573ac6f80
Ichiro Suzuki <ichiro.suzuki@miraclelinux.com>
2017-08-17 13:41:13 +0900

    Style fix: now, pep8 compliant

--- porting_helper.py
+++ porting_helper.py
@@ -1,142 +1,147 @@
 from git import Repo, Commit
 import subprocess
-import re	# Regular Expression
-import abc	# Abstruct Base Class
+import re    # Regular Expression
+import abc   # Abstruct Base Class
 
 __all__ = [
-	'PortingHelper',
-	'RevertFilter',
-	'PatchIdFilter',
-	'SummaryFilter',
+    'PortingHelper',
+    'RevertFilter',
+    'PatchIdFilter',
+    'SummaryFilter',
 ]
 
+
 class CommitWithId():
-	patchid = b''
+    patchid = b''
 
-	def __init__(self, commit):
-		self.commit = commit
-		self.patchid = self.set_patchid(commit.hexsha)
+    def __init__(self, commit):
+        self.commit = commit
+        self.patchid = self.set_patchid(commit.hexsha)
 
-	def set_patchid(self, hexsha):
-		command = 'git show ' + hexsha + '| git patch-id'
-		outstr = subprocess.Popen(
-				command,
-				cwd=self.commit.repo.working_dir,
-				stdout=subprocess.PIPE,
-				shell=True
-				).communicate()[0]
-		return outstr.split()[0]
+    def set_patchid(self, hexsha):
+        command = 'git show ' + hexsha + '| git patch-id'
+        outstr = subprocess.Popen(command,
+                                  cwd=self.commit.repo.working_dir,
+                                  stdout=subprocess.PIPE,
+                                  shell=True
+                                  ).communicate()[0]
+        return outstr.split()[0]
 
-	def __getattr__(self, name):
-		return getattr(self.commit, name)
+    def __getattr__(self, name):
+        return getattr(self.commit, name)
+
 
 class Filter(metaclass=abc.ABCMeta):
-	@abc.abstractmethod
-	def action(self, commit):
-		return True
+    @abc.abstractmethod
+    def action(self, commit):
+        return True
 
-	@abc.abstractmethod
-	def get_results(self):
-		return []
+    @abc.abstractmethod
+    def get_results(self):
+        return []
+
 
 class RevertFilter(Filter):
-	revert_list = []
-	revert_commit_expr = re.compile(
-			"This reverts\n? '?commit ([0-9a-f]*)", re.MULTILINE)
+    revert_list = []
+    revert_commit_expr = re.compile(
+            "This reverts\n? '?commit ([0-9a-f]*)", re.MULTILINE)
 
-	def get_reverted(self, message):
-		matched = self.revert_commit_expr.search(message)
+    def get_reverted(self, message):
+        matched = self.revert_commit_expr.search(message)
 
-		if (matched is None):
-			return '--'
+        if (matched is None):
+            return '--'
 
-		return matched.group(1)
+        return matched.group(1)
 
-	def action(self, commit):
-		if (commit.summary.startswith('Revert "')):
-			reverted = self.get_reverted(commit.message)
-			self.revert_list.append([commit.hexsha, reverted, False])
+    def action(self, commit):
+        if (commit.summary.startswith('Revert "')):
+            reverted = self.get_reverted(commit.message)
+            self.revert_list.append([commit.hexsha, reverted, False])
 
-		for r in self.revert_list:
-			if (commit.hexsha.startswith(r[1])):
-				r[2] = True
+        for r in self.revert_list:
+            if (commit.hexsha.startswith(r[1])):
+                r[2] = True
 
-		return True
+        return True
 
-	def get_results(self):
-		return self.revert_list
+    def get_results(self):
+        return self.revert_list
+
 
 class PatchIdFilter(Filter):
-	patchid_set = set();
+    patchid_set = set()
 
-	def __init__(self, commits):
-		for c in commits:
-			self.patchid_set.add(c.patchid)
+    def __init__(self, commits):
+        for c in commits:
+            self.patchid_set.add(c.patchid)
 
-	def action(self, commit):
-		if (commit.patchid in self.patchid_set):
-			return False
+    def action(self, commit):
+        if (commit.patchid in self.patchid_set):
+            return False
 
-		return True
+        return True
 
-	def get_results(self):
-		l = []
+    def get_results(self):
+        l = []
 
-		for i in self.patchid_set:
-			l.append(i)
+        for i in self.patchid_set:
+            l.append(i)
 
-		return l
+        return l
+
 
 class SummaryFilter(Filter):
-	summary_set = set();
+    summary_set = set()
 
-	def __init__(self, commits):
-		for c in commits:
-			self.summary_set.add(c.summary)
+    def __init__(self, commits):
+        for c in commits:
+            self.summary_set.add(c.summary)
 
-	def action(self, commit):
-		if (commit.summary in self.summary_set):
-			return False
+    def action(self, commit):
+        if (commit.summary in self.summary_set):
+            return False
 
-		return True
+        return True
 
-	def get_results(self):
-		l = []
+    def get_results(self):
+        l = []
 
-		for i in self.summary_set:
-			l.append(i)
+        for i in self.summary_set:
+            l.append(i)
 
-		return l
+        return l
+
 
 class PortingHelper:
-	repository = ''
+    repository = ''
 
-	def __init__(self, repo='.'):
-		self.repository = Repo(repo)
+    def __init__(self, repo='.'):
+        self.repository = Repo(repo)
 
-	def dir(self):
-		return self.repository.working_dir
+    def dir(self):
+        return self.repository.working_dir
 
-	def commits(self, rev='HEAD', paths='.', filters=[]):
-		commit_list = []  # list of list(git.Commit, patchid)
+    def commits(self, rev='HEAD', paths='.', filters=[]):
+        commit_list = []  # list of list(git.Commit, patchid)
 
-		for c in self.repository.iter_commits(rev=rev, paths=paths):
-			if (len(c.parents) > 1):
-				continue  # skip merge commits
+        for c in self.repository.iter_commits(rev=rev, paths=paths):
+            if (len(c.parents) > 1):
+                continue  # skip merge commits
 
-			c_id = CommitWithId(c)
+            c_id = CommitWithId(c)
 
-			keep = True
-			for f in filters:
-				keep &= f.action(c_id)
+            keep = True
+            for f in filters:
+                keep &= f.action(c_id)
 
-			if (keep):
-				commit_list.append(c_id)
+            if (keep):
+                commit_list.append(c_id)
 
-		results = [commit_list]
-		for f in filters:
-			results.append(f.get_results())
+        results = [commit_list]
+        for f in filters:
+            results.append(f.get_results())
 
-		return results
+        return results
 
-# vim: set shiftwidth=3 tabstop=3 :
+# vim: set shiftwidth=4 tabstop=99 :
--- tests/test_porting_helper.py
+++ tests/test_porting_helper.py
@@ -6,105 +6,106 @@ import subprocess
 
 from porting_helper import *
 
+
 class TestPortingHelper(TestCase):
-	repo_path = './tests/foo'
-	message_buf = []
+    repo_path = './tests/foo'
+    message_buf = []
 
-	@classmethod
-	def setUpClass(klass):
-		subprocess.call(['tar', 'xzf', './tests/foo.tar.gz', '-C', './tests'])
-		pass
+    @classmethod
+    def setUpClass(klass):
+        subprocess.call(['tar', 'xzf', './tests/foo.tar.gz', '-C', './tests'])
+        pass
 
-	@classmethod
-	def tearDownClass(klass):
-		print()
-		for l in TestPortingHelper.message_buf:
-			print(l)
-		subprocess.call(['rm', '-rf', './tests/foo'])
-		pass
+    @classmethod
+    def tearDownClass(klass):
+        print()
+        for l in TestPortingHelper.message_buf:
+            print(l)
+        subprocess.call(['rm', '-rf', './tests/foo'])
+        pass
 
-	def setUp(self):
-		self.target = PortingHelper(TestPortingHelper.repo_path)
+    def setUp(self):
+        self.target = PortingHelper(TestPortingHelper.repo_path)
 
-	def tearDown(self):
-		pass
+    def tearDown(self):
+        pass
 
-	def test_dir(self):
-		self.assertEqual(
-				self.target.dir(),
-				path.abspath(TestPortingHelper.repo_path))
+    def test_dir(self):
+        self.assertEqual(
+                self.target.dir(),
+                path.abspath(TestPortingHelper.repo_path))
 
-	def test_commits(self):
-		l = self.target.commits()
-		self.assertEqual(len(l), 1)
-		commits = l[0]
-		self.assertEqual(len(commits), 5)
-		self.message_buf.append('>>>> commits')
-		for c in commits:
-			self.message_buf.append('%s %s' % (c.patchid, c.summary))
-		self.message_buf.append('<<<<')
+    def test_commits(self):
+        l = self.target.commits()
+        self.assertEqual(len(l), 1)
+        commits = l[0]
+        self.assertEqual(len(commits), 5)
+        self.message_buf.append('>>>> commits')
+        for c in commits:
+            self.message_buf.append('%s %s' % (c.patchid, c.summary))
+        self.message_buf.append('<<<<')
 
-	def test_commits_partial(self):
-		l = self.target.commits(rev='HEAD~2..HEAD')
-		self.assertEqual(len(l), 1)
-		commits = l[0]
-		self.assertEqual(len(commits), 2)
+    def test_commits_partial(self):
+        l = self.target.commits(rev='HEAD~2..HEAD')
+        self.assertEqual(len(l), 1)
+        commits = l[0]
+        self.assertEqual(len(commits), 2)
 
-	def test_commits_branch(self):
-		l = self.target.commits(rev='dev_b~3..dev_b')
-		self.assertEqual(len(l), 1)
-		commits = l[0]
-		self.assertEqual(len(commits), 3)
+    def test_commits_branch(self):
+        l = self.target.commits(rev='dev_b~3..dev_b')
+        self.assertEqual(len(l), 1)
+        commits = l[0]
+        self.assertEqual(len(commits), 3)
 
-	def test_commits_paths(self):
-		commits = self.target.commits(rev='dev_b')[0]
-		self.assertEqual(len(commits), 7) # all to the beginning
-		commits = self.target.commits(rev='dev_b', paths=['b.txt'])[0]
-		self.assertEqual(len(commits), 2) # only about 'b.txt'
+    def test_commits_paths(self):
+        commits = self.target.commits(rev='dev_b')[0]
+        self.assertEqual(len(commits), 7)  # all to the beginning
+        commits = self.target.commits(rev='dev_b', paths=['b.txt'])[0]
+        self.assertEqual(len(commits), 2)  # only about 'b.txt'
 
-	def test_filter_revert(self):
-		l = self.target.commits(filters=[RevertFilter()])
-		self.assertEqual(len(l), 2)
-		reverts = l[1]
-		self.assertEqual(len(reverts), 1)
-		self.message_buf.append('>>>> reverts')
-		for c in reverts:
-			self.message_buf.append('%s %s %s' % tuple(c))
-		self.message_buf.append('<<<<')
-		self.assertEqual(reverts[0][2], True)
+    def test_filter_revert(self):
+        l = self.target.commits(filters=[RevertFilter()])
+        self.assertEqual(len(l), 2)
+        reverts = l[1]
+        self.assertEqual(len(reverts), 1)
+        self.message_buf.append('>>>> reverts')
+        for c in reverts:
+            self.message_buf.append('%s %s %s' % tuple(c))
+        self.message_buf.append('<<<<')
+        self.assertEqual(reverts[0][2], True)
 
-	def test_filter_patchid(self):
-		l = self.target.commits(rev='dev_b~3..dev_b')
-		pif = PatchIdFilter(l[0])
-		ids = pif.get_results()
-		self.assertEqual(len(ids), 3)
-		self.message_buf.append('>>>> patch ids')
-		for i in ids:
-			self.message_buf.append(i)
-		self.message_buf.append('<<<<')
+    def test_filter_patchid(self):
+        l = self.target.commits(rev='dev_b~3..dev_b')
+        pif = PatchIdFilter(l[0])
+        ids = pif.get_results()
+        self.assertEqual(len(ids), 3)
+        self.message_buf.append('>>>> patch ids')
+        for i in ids:
+            self.message_buf.append(i)
+        self.message_buf.append('<<<<')
 
-		l = self.target.commits(filters=[pif])
-		self.assertEqual(len(l), 2)
-		commits = l[0]
-		ids = l[1]
-		self.assertEqual(len(ids), 3)
-		self.assertEqual(len(commits), 4) # One less than all
+        l = self.target.commits(filters=[pif])
+        self.assertEqual(len(l), 2)
+        commits = l[0]
+        ids = l[1]
+        self.assertEqual(len(ids), 3)
+        self.assertEqual(len(commits), 4)  # One less than all
 
-	def test_filter_summary(self):
-		l = self.target.commits(rev='dev_b~3..dev_b')
-		pif = SummaryFilter(l[0])
-		ids = pif.get_results()
-		self.assertEqual(len(ids), 3)
-		self.message_buf.append('>>>> summary lines')
-		for i in ids:
-			self.message_buf.append(i)
-		self.message_buf.append('<<<<')
+    def test_filter_summary(self):
+        l = self.target.commits(rev='dev_b~3..dev_b')
+        pif = SummaryFilter(l[0])
+        ids = pif.get_results()
+        self.assertEqual(len(ids), 3)
+        self.message_buf.append('>>>> summary lines')
+        for i in ids:
+            self.message_buf.append(i)
+        self.message_buf.append('<<<<')
 
-		l = self.target.commits(filters=[pif])
-		self.assertEqual(len(l), 2)
-		commits = l[0]
-		ids = l[1]
-		self.assertEqual(len(ids), 3)
-		self.assertEqual(len(commits), 4) # One less than all
+        l = self.target.commits(filters=[pif])
+        self.assertEqual(len(l), 2)
+        commits = l[0]
+        ids = l[1]
+        self.assertEqual(len(ids), 3)
+        self.assertEqual(len(commits), 4)  # One less than all
 
-# vim: set tabstop=3 shiftwidth=3 :
+# vim: set shiftwidth=4 tabstop=99 :
